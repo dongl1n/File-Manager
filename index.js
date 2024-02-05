b8f9e6be2e6eca3f67d9655b244115cebebfd001
Juja <nastya341151@gmail.com>
2024-02-06 01:10:49 +0300

    feat: add func

--- index.js
+++ index.js
@@ -1,30 +1,106 @@
-
-import { homedir } from 'node:os';
+import os from 'node:os';
 import path from 'node:path';
 import fs from 'node:fs';
 import {createHash} from 'node:crypto';
-import {createReadStream} from 'node:fs';
+import {createReadStream, createWriteStream} from 'node:fs';
 import process from 'node:process';
 import { fileURLToPath } from 'url';
+import { createGzip, createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
 
 /* ----------------------------------------------- GLOBAL ----------------------------------------------- */
 const __filename = fileURLToPath(import.meta.url);
 let currentDir;
+let command = {
+  ".exit":"Go out",
+  "home":"Go home directory",
+  "up":"Go up a level",
+  "cd path_to_directory": "go to directory",
+  "ls": "print files and directory in current directory",
+  "cat path_to_file": "read and print file in directory",
+  "add new_file_name": "add file in directory",
+  "rn path_to_file new_filename": "rename file",
+  "cp path_to_file path_to_new_directory": "copy file",
+  "mv path_to_file path_to_new_directory": "move file to other directory",
+  "rm path_to_file": "remove file",
+  "os --EOL": "print EOL",
+  "os --cpus": "print cpu info",
+  "os --homedir": "print home directory",
+  "os --username": "print username in os",
+  "os --architecture": "print architecture info",
+  "hash path_to_file": "print hash for file",
+  "compress path_to_file path_to_destination": "compress file",
+  "decompress path_to_file path_to_destination": "decompress file",
+  "help": "print command list"
+};
 
 
 class Manager{
   start(){
     currentDir = path.dirname(__filename);
+    console.log("Command list:")
+    this.command()
     this.print();
   }
   print(){
     console.log(`You are currently in ${currentDir.toLowerCase()}`);
+    console.log("enter the command")
+  }
+  command(){
+    console.table(command);
+  }
+  os(param){
+    switch(param.trim()){
+      case "--EOL":{
+        console.log(JSON.stringify(os.EOL));
+        break;
+      }
+
+      case "--cpus":{
+        let objs = os.cpus();
+        let counter = 0;
+        let result = [];
+        console.log("number of CPUs: "+objs.length)
+        for(let obj of objs){
+          let o = {};
+          o.model = obj.model;
+          o.speed = obj.speed/1000;
+          result[counter] = o;
+          counter++;
+        }
+        console.table(result);
+        break;
+      }
+
+      case "--homedir":{
+        console.log("Home directory: " + os.homedir());
+        break;
+      }
+
+      case "--username":{
+        console.log("User name: "+os.userInfo().username);
+        break;
+      }
+
+      case "--architecture":{
+        console.log("Architecture: " +os.arch());
+        break;
+      }
+      default: {
+        console.log("Error: invalid input");
+      }
+
+  
+    }
+
+    this.print();
+
   }
 }
 
 class Directory extends Manager{
   home(){
-    currentDir = homedir();
+    currentDir = os.homedir();
     this.print();
   }
   
@@ -127,18 +203,143 @@ class File extends Manager{
     this.print();
   }
 
-  hash(pathFile){
-    const hash = createHash('sha256');
-    const input = createReadStream(pathFile.toLowerCase());
+  cat(pathFile){
+    const input = createReadStream(pathFile.trim(), {encoding: 'utf-8'});
+    console.log("Read "+pathFile);
     input.on('readable', () => {
-      const data = input.read();
-      if (data)
-          hash.update(data);
-      else {
-          console.log(`${hash.digest('hex')}`);
+      let data;
+
+      while ((data = input.read()) !== null) {
+        console.log(data);
       }
     });
-    this.print();
+    input.on('end', ()=>{
+      console.log("file's end");
+      this.print();
+    })
+  }
+
+  rn(pathsFile){
+    let oldFile = pathsFile.split(" ")[0].trim();
+    let newFile = pathsFile.split(" ")[1].trim();
+    let dir = oldFile.replace(/\w{0,}.\w{0,}$/, "");
+    if(!fs.existsSync(oldFile)){
+      console.log("The file does not exist");
+      return;
+    }
+    if(fs.existsSync(dir+newFile)){
+      console.log("The file does exist");
+      return;
+    }
+    fs.rename(oldFile, dir+newFile, err => {
+      if(err) console.log("Operation failed: "+err); // не удалось переименовать файл
+      else console.log('Operation completed');
+      this.print();
+    }); 
+  }
+
+  rm(pathFile){
+    if (!fs.existsSync(pathFile.trim())){
+      console.log("FS operation failed: file do not exist");
+      return;
+    }
+    fs.unlink(pathFile.trim(), err => {
+        if(err) console.log("Operation failed: "+err);
+        console.log('Operation completed');
+        this.print();
+    });
+  }
+
+  cp(pathsFile){
+    let oldFile = pathsFile.split(" ")[0].trim();
+    let newFile = pathsFile.split(" ")[1].trim();
+    let fileName = oldFile.match(/\w{0,}.\w{0,}$/)[0];
+    let data;
+    const input = createReadStream(oldFile, {encoding: 'utf-8'});
+    console.log(oldFile)
+    console.log(newFile+"/"+fileName)
+    console.log("Read "+oldFile.trim());
+    input.on('readable', () => {
+
+    while ((data = input.read()) !== null) {
+        const writer = createWriteStream(newFile+"/"+fileName);
+        writer.write(data);
+    }
+    });
+    input.on('end', ()=>{
+      console.log("Operation completed");
+      this.print();
+      return 1;
+    })
+  }
+
+  mv(pathsFile){
+    let oldFile = pathsFile.split(" ")[0].trim();
+    let newFile = pathsFile.split(" ")[1].trim();
+    let fileName = oldFile.match(/\w{0,}.\w{0,}$/)[0];
+    if(!fs.existsSync(oldFile)){
+      console.log("The file does not exist");
+      return;
+    }
+    if(fs.existsSync(newFile+"/"+fileName)){
+      console.log("The file does exist");
+      return;
+    }
+    this.cp(pathsFile);
+
+    const interval = setInterval(()=>{
+      if(fs.existsSync(newFile+"/"+fileName)){
+        this.rm(oldFile);
+        this.print();
+        clearInterval(interval);
+      }
+    }, 100)
+    
+  }
+
+  hash(pathFile){
+    const hash = createHash('sha256');
+    const input = createReadStream(pathFile.trim());
+    input.on('readable', () => {
+      const data = input.read();
+      if (data) hash.update(data);
+      else {
+          console.log(`hash: ${hash.digest('hex')}`);
+          this.print();
+      }
+    });
+  }
+
+  compress(pathsFile){
+    let oldFile = pathsFile.split(" ")[0].trim();
+    let newFile = pathsFile.split(" ")[1].trim();
+    let fileName = oldFile.match(/\w{0,}.\w{0,}$/)[0].replace(/.\w{0,}$/, "");
+
+    console.log(oldFile)
+    const gzip = createGzip();
+    const source = createReadStream(oldFile);
+    const destination = createWriteStream(newFile+"/"+fileName+'.gz');
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
+  }
+
+  decompress(pathsFile){
+    let oldFile = pathsFile.split(" ")[0].trim();
+    let newFile = pathsFile.split(" ")[1].trim();
+    let fileName = oldFile.match(/\w{0,}.\w{0,}$/)[0].replace(/.\w{0,}$/, "");
+    const gzip = createUnzip();
+    const destination = createWriteStream(newFile +"/"+ fileName+".txt");
+    const source = createReadStream(oldFile);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) console.log(err);
+        process.exitCode = 1;
+    });
   }
 }
 
@@ -160,6 +361,10 @@ const write = async () => {
           process.exit(0);
           return;
         }
+        case "help":{
+          m.command()
+          break;
+        }
         case "home":{
           dir.home();
           break;
@@ -177,18 +382,60 @@ const write = async () => {
             dir.cd(data.split(' ')[1]);
             break;
           }
-          if(data.startsWith("add ") && data.length > 6 && data.split(' ').length-1 === 1){
+          else if(data.startsWith("os ") && data.length > 5 && data.split(' ').length-1 === 1){
+            m.os(data.split(' ')[1]);
+            break;
+          }
+          else if(data.startsWith("rn ") && data.length > 5){
+            let pathsFile = data.slice(3);
+            file.rn(pathsFile);
+            break;
+          }
+          else if(data.startsWith("cp ") && data.length > 5){
+            let pathsFile = data.slice(3);
+            file.cp(pathsFile);
+            break;
+          }
+          else if(data.startsWith("mv ") && data.length > 5){
+            let pathsFile = data.slice(3);
+            file.mv(pathsFile);
+            break;
+          }
+          else if(data.startsWith("compress ") && data.length > 5){
+            let pathsFile = data.slice(9);
+            file.compress(pathsFile);
+            break;
+          }
+          else if(data.startsWith("decompress ") && data.length > 5){
+            let pathsFile = data.slice(11);
+            file.decompress(pathsFile);
+            break;
+          }
+          else if(data.startsWith("rm ") && data.length > 5){
+            let pathFile = data.slice(3);
+            file.rm(pathFile);
+            break;
+          }
+          else if(data.startsWith("add ") && data.length > 6 && data.split(' ').length-1 === 1){
             let fileName = data.slice(4);
             if(fileName.includes(".")) file.add(fileName);
             else "Error: invalid input";
             break;
           }
-        }
-        if(data.startsWith("hash ") && data.length > 7){
-          let fileName = data.slice(5);
-          console.log("ПУТЬ" + fileName)
-          file.hash(fileName)
-          break;
+          else if(data.startsWith("cat ") && data.length > 6 && data.split(' ').length-1 === 1){
+            let fileName = data.slice(4);
+            if(fileName.includes(".")) file.cat(fileName);
+            else "Error: invalid input";
+            break;
+          }
+          else if(data.startsWith("hash ") && data.length > 7){
+            let fileName = data.slice(5);
+            file.hash(fileName)
+            break;
+          }
+          else{
+            console.log("Error: invalid input");
+          }
         }
       }
   })
